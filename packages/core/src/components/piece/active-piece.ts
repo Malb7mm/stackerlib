0abf4702ef40a3f782d482ac9f72c47fe1d98613
Malb7mm <ykmeak9g@gmail.com>
2025-08-19 06:06:56 +0900

    change: give initial values

--- packages/core/src/components/piece/active-piece.ts
+++ packages/core/src/components/piece/active-piece.ts
@@ -2,7 +2,7 @@ import { PieceShape } from "@/components/piece/piece-shape.js";
 import { PieceDirection } from "@/components/piece/types.js";
 
 export class ActivePiece<TPieceKey> {
-  private _x: number;
+  private _x: number = 0;
   get x() { return this._x; };
 
   set x(value) { 
@@ -12,7 +12,7 @@ export class ActivePiece<TPieceKey> {
     this._x = value;
   };
 
-  private _y: number;
+  private _y: number = 0;
   get y() { return this._y; };
 
   set y(value) { 
@@ -22,11 +22,11 @@ export class ActivePiece<TPieceKey> {
     this._y = value;
   };
 
-  private _shape: PieceShape<TPieceKey>;
+  private _shape: PieceShape<TPieceKey> = undefined;
   get shape() { return this._shape; };
   set shape(value) { this._shape = value; };
 
-  private _direction: PieceDirection;
+  private _direction: PieceDirection = 0;
   get direction() { return this._direction; };
   set direction(value) { this._direction = value; };
 }

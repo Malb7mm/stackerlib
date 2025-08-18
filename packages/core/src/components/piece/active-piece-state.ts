96b749194f2bf429fb13c517ae5debf804004288
Malb7mm <ykmeak9g@gmail.com>
2025-08-19 07:12:52 +0900

    feat: ActivePieceState emit event "active-piece-moved"

--- packages/core/src/components/piece/active-piece-state.ts
+++ packages/core/src/components/piece/active-piece-state.ts
@@ -1,7 +1,26 @@
+import { ActivePieceMovedContext } from "@/components/event/contexts.js";
+import { EventEmitter } from "@/components/event/event-bus.js";
 import { PieceShape } from "@/components/piece/piece-shape.js";
 import { PieceDirection } from "@/components/piece/types.js";
 
-export class ActivePiece<TPieceKey> {
+export class ActivePieceState<TPieceKey> {
+  private _emitter: EventEmitter<ActivePieceMovedContext<TPieceKey>>;
+
+  constructor(options: {
+    activePieceMovedEmitter: EventEmitter<ActivePieceMovedContext<TPieceKey>>,
+  }) {
+    this._emitter = this._emitter;
+  }
+
+  private _emitEvent() {
+    this._emitter.emit({
+      x: this._x,
+      y: this._y,
+      shape: this._shape,
+      direction: this._direction,
+    });
+  }
+
   private _x: number = 0;
   get x() { return this._x; };
 
@@ -9,7 +28,12 @@ export class ActivePiece<TPieceKey> {
     if (!Number.isInteger(value)) {
       throw new Error(`X-coordinate must be integer, but tried to set ${value}.`);
     }
+    const updated = this._x != value;
     this._x = value;
+
+    if (updated) {
+      this._emitEvent();
+    }
   };
 
   private _y: number = 0;
@@ -19,14 +43,33 @@ export class ActivePiece<TPieceKey> {
     if (!Number.isInteger(value)) {
       throw new Error(`Y-coordinate must be integer, but tried to set ${value}.`);
     }
+    const updated = this._y != value;
     this._y = value;
+
+    if (updated) {
+      this._emitEvent();
+    }
   };
 
   private _shape: PieceShape<TPieceKey> = undefined;
   get shape() { return this._shape; };
-  set shape(value) { this._shape = value; };
+  set shape(value) { 
+    const updated = this._shape != value;
+    this._shape = value;
+
+    if (updated) {
+      this._emitEvent();
+    }
+  };
 
   private _direction: PieceDirection = 0;
   get direction() { return this._direction; };
-  set direction(value) { this._direction = value; };
+  set direction(value) { 
+    const updated = this._direction != value;
+    this._direction = value;
+
+    if (updated) {
+      this._emitEvent();
+    }
+  };
 }

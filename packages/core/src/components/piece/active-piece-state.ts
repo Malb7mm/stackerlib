894ddbf38cb7086de524cfd410b4cbe3d62e4d9c
Malb7mm <ykmeak9g@gmail.com>
2025-08-21 09:44:41 +0900

    docs: JSDoc

--- packages/core/src/components/piece/active-piece-state.ts
+++ packages/core/src/components/piece/active-piece-state.ts
@@ -7,7 +7,7 @@ export class ActivePieceState {
   private _emitter: EventEmitter<ActivePieceMovedContext>;
 
   constructor({ emitter }: {
-    /** Event emitted when the state changes. It accepts "active-piece-moved" ({@link ActivePieceMovedContext} event emitter.) */
+    /** Event emitted when the state changes. It accepts "active-piece-moved" {@link ActivePieceMovedContext} event emitter. */
     emitter: EventEmitter<ActivePieceMovedContext>,
   }) {
     this._emitter = emitter;
